b10da7b39582e017d42c63a0e37b140629fa4373
HMKL <catalin.bac@gmail.com>
2020-04-24 13:49:38 +0300

    Add files via upload

--- timer.js
+++ timer.js
@@ -23,14 +23,15 @@ function scrollFunction() {
         $('#mainNav').css('backgroundColor', '#fafafa'); 
         $('.navbar-nav.mr-auto > li > a ').css('color', '#000');
         $('#time').css('color', 'black');
-        $('#mainNav').css('opacity', '0.5');
-        // $('#mainNav').css("padding", "0px 10px 0 10px");
         $('#mainNav').css("padding-top", "0px");
         $('#mainNav').css("padding-bottom", "0px");
+        $('#mainNav').css('box-shadow', '0px 0px 10px black');
+        $('#mainNav').css("transition", "background-color' '0.2s ease");
+      
     } else {
         $('#mainNav').css('backgroundColor', 'transparent');
         $('.navbar-nav.mr-auto > li > a ').css('color', 'white');
-        $('#time ').css('color', 'white');
+        $('#time').css('color', 'white');
         $("#mainNav").removeAttr("style");
         $('#mainNav').css("padding-top", "0px");
         $('#mainNav').css("padding-bottom", "0px");
@@ -40,3 +41,36 @@ function scrollFunction() {
 
 	
 $(".navbar-nav.mr-auto > li > a").css('color', 'white');
+
+$(document).ready(function(){
+  $("a").click(function(){
+    $(".clickme").css('color', '#000');
+    $(".clickme").css('backgroundColor', '#f4623a');
+    $('.clickme:hover').css('border', 'none');
+    $('.clickme:hover').css('box-shadow', 'none');
+
+  });
+});
+
+
+
+// fade div content...
+ 
+// $(function () {
+  
+//   $('#btn1').on('click', function () {
+//     $('#test').fadeToggle(200);
+//   });
+
+// });
+
+
+$(function () {
+  $('#btn2').on('click', function () {
+    $('#jum').fadeToggle(1000);
+  });
+});
+
+$(window).on('beforeunload', function(){
+  $(window).scrollTop(0);
+});
